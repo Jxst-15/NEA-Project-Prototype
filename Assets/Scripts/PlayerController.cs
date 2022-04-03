b4b672de52f31cc4d17072aa3f7cf7f748503da6
Justin Jay Villapa <justinjay2005@yahoo.co.uk>
2022-04-03 22:06:34 +0100

    Added comments to PlayerController script
    Added basic comments to know what each does

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float _speed = 2f;
-    public float JumpForce = 1f;
+    public float _speed = 2f; //Decides how fast player moves
+    public float JumpForce = 1f; //Decides how high player jumps
 
     private Rigidbody2D _rigidbody;
 
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody = GetComponent<Rigidbody2D>(); //Creates reference to Rigid Body
     }
 
     void Update()
     {
-        var _move = Input.GetAxis("Horizontal");
-        transform.position = transform.position + new Vector3(_move * _speed * Time.deltaTime, 0, 0);
+        var _move = Input.GetAxis("Horizontal"); //Moving left and right
+        transform.position = transform.position + new Vector3(_move * _speed * Time.deltaTime, 0, 0); //Actual character moving
 
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f) //Jumping
         {
-            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse); //Mechanic for jumping
         }
     }
 }
