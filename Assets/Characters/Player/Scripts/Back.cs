43c8d37e9d3eea7edf97297d9537248383ceccf9
Justin Jay Villapa <justinjay2005@yahoo.co.uk>
2022-04-28 19:37:16 +0100

    Still working on block

--- Assets/Characters/Player/Scripts/Back.cs
+++ Assets/Characters/Player/Scripts/Back.cs
@@ -1,15 +1,33 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Back : MonoBehaviour
 {
-    public LayerMask enemyLayers;
-    private bool enemyBehind = false;
+    public bool enemyBehind = false;
+
+    Collider2D[] enemiesBehind = new Collider2D[4];
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //var TriggerList : List.< Collider > = new List.< Collider > ();
         enemyBehind = true;
         Debug.Log("Enemy behind");
+        //for (int i = 0; i < 4; i++)
+        //{
+        //    enemiesBehind[i] = collision;
+        //}
+        //foreach (Collider2D enemy in enemiesBehind)
+        //{
+        //    //gameObject.GetComponentInParent<PlayerStats>().TakingDmgBack(enemy.GetComponent<EnemyAI>().LDamage);
+        //    Debug.Log("Player hit (Light) (Behind)");
+        //}
     }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        enemyBehind = false;
+        //for (int i = 0; i < 4; i++)
+        //{
+        //    enemiesBehind[i] = null;
+        //}
+    }
+
 }
